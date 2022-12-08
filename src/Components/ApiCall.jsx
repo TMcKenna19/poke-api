971cdf160794d9640f044037e49ac6eb8b526885
tmckenna19 <tdlmckenna@gmail.com>
2022-12-08 16:22:19 -0600

    useEffect

--- src/Components/ApiCall.jsx
+++ src/Components/ApiCall.jsx
@@ -1,17 +1,26 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
  const ApiCall = props => {
-    const [pokemon, setPokemon] = useState({});
-    axios.get("https://pokeapi.co/api/v2/pokemon/charmander")
-        .then(res => setPokemon(res.data))
+    const [pokemon, setPokemon] = useState(null);
+    useEffect(() => {
+        axios.get("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+            .then(res => setPokemon(res.data))
+    }, [])
+     // ^ this is called a dependency array, any time somthing in the array is changed, it triggers useEffect. If forgotten useEffect will call infinitely. 
     return(
-        <div>
-            <h2>My API Call</h2>
-            <img src={pokemon.sprites.front_default} alt="sprite" />
-            <h3>Name: {pokemon.name}</h3>
+        <div className='form-control' >
+            <h4>Catch them all</h4>
+            {
+                pokemon ? <>
+                <img src={pokemon.sprites.front_default} alt="sprite" />
+                <h5>{pokemon.name}</h5>
+                </> : ""
+            }
+            <button type="button" className="btn btn-outline-dark mt-2 mb-2">Fetch Pokemon</button>
         </div>
+
     )
  };
 
